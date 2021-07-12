16e26e92f1fef602b6b7a1c89349c456a909a82e
mariamalrashidi <m.j.alrashidi@gmail.com>
2021-07-12 07:49:42 +0300

    first commit

---
+++ routes/article.js
@@ -0,0 +1,82 @@
+// All our Article related routes will go here in this file
+
+const express = require('express');
+
+const router = express.Router();
+
+const moment = require('moment');
+
+var methodOverride = require('method-override');
+
+// Use method override
+router.use(methodOverride('_method'));
+
+// Import Model
+const Article = require("../models/Article");
+
+// Grab the form data
+router.use(express.urlencoded({extended: true}));
+
+// HTTP GET - Retrive data from the database - R 
+// HTTP POST - To send the data into the database - C - Done
+// HTTP PUT - TO update the data into the database - U
+// HTTP DELETE - To delete the data from the database - D
+
+
+// HTTP GET - Load an Article Form
+router.get("/article/add", (req, res) => {
+    res.render("article/add");
+})
+
+// HTTP POST - To post the Article
+router.post("/article/add", (req, res) => {
+
+    let article = new Article(req.body);
+
+    // Save the data to the database
+    article.save()
+    .then(() => {
+        res.redirect("/article/index");
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send("ERROR!!!");
+    })
+})
+
+// HTTP GET - Article Index
+router.get("/article/index", (req, res) => {
+    // Find all Articles
+    Article.find()
+    .then(articles => {
+        res.render("article/index", {articles, moment});
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+// HTTP GET - Article by ID
+router.get("/article/detail", (req, res) => {
+    console.log(req.query.id);
+    Article.findById(req.query.id)
+    .then(article => {
+        res.render("article/detail", {article, moment});
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+// HTTP DELETE - Article delete
+router.delete("/article/delete", (req, res) => {
+    Article.findByIdAndDelete(req.query.id)
+    .then(()=>{
+        res.redirect("/article/index");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const PORT = 4000;
+
+const expresslayouts = require("express-ejs-layouts");
+
+// Initialize express
+const app = express();
+
+// Look for static files here in this folder
+app.use(express.static("public"));
+
+// Look into the views folder for layout.ejs file
+app.use(expresslayouts);
+
+// Import Routes
+const indexRoute = require('./routes/index');
+const articleRoute = require('./routes/article');
+
+// Mount Routes
+app.use('/', indexRoute);
+app.use('/', articleRoute);
+
+// Setting view engine to ejs.
+// Node.js to look into the folder views for all ejs files
+app.set("view engine", "ejs");
+
+mongoose.connect(
+  "mongodb://localhost:27017/blogapp",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Mongodb connected seccessfully!!!");
+  }
+);
+
+// Listen for HTTP request on PORT 4000
+app.listen(PORT, () => {
+  console.log(`Running on PORT  ${PORT}`);
+});
